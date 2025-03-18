e39ca69e82378d6a03efc677da950343ac0a7068
eunji <dmswl2781@naver.com>
2025-03-18 21:30:10 +0900

    0318_1

--- homework/0317/5204_병합정렬.py
+++ homework/0317/5204_병합정렬.py
@@ -1,42 +1,42 @@
-# def merge(left, right):
-#     global ct
-#     result = [0] * (len(left) + len(right))
-#     l = r = 0
-#     
-#     if left[-1] > right[-1]:
-#         ct += 1
-#         
-#     while l < len(left) and r < len(right):
-#         if left[l] < right[r]:
-#             result[l + r] = left[l]
-#             l += 1
-#         else:
-#             result[l + r] = right[r]
-#             r += 1
-#     
-#     while l < len(left):
-#         result[l + r] = left[l]
-#         l += 1
-#     while r < len(right):
-#         result[l + r] = right[r]
-#         r += 1
-#         
-#     return result 
-#     
-# def merge_sort(li):
-#     if len(li) == 1:
-#         return li
-#     
-#     mid = len(li) // 2
-#     left_list = merge_sort(li[:mid])
-#     right_list = merge_sort(li[mid:])
-#     return merge(left_list, right_list)
-#
+def merge(left, right):
+    global ct
+    result = [0] * (len(left) + len(right))
+    l = r = 0
 
-# T = int(input())
-# for tc in range(1, T+1):
-#     N = int(input())
-#     arr = list(map(int, input().split()))
-#     ct = 0
-#     sort_arr = merge_sort(arr)
-#     print(f'#{tc} {sort_arr[N//2]} {ct}')
+    if left[-1] > right[-1]:
+        ct += 1
+
+    while l < len(left) and r < len(right):
+        if left[l] < right[r]:
+            result[l + r] = left[l]
+            l += 1
+        else:
+            result[l + r] = right[r]
+            r += 1
+
+    while l < len(left):
+        result[l + r] = left[l]
+        l += 1
+    while r < len(right):
+        result[l + r] = right[r]
+        r += 1
+
+    return result 
+
+def merge_sort(li):
+    if len(li) == 1:
+        return li
+
+    mid = len(li) // 2
+    left_list = merge_sort(li[:mid])
+    right_list = merge_sort(li[mid:])
+    return merge(left_list, right_list)
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    ct = 0
+    sort_arr = merge_sort(arr)
+    print(f'#{tc} {sort_arr[N//2]} {ct}')
--- homework/0317/5205_퀵정렬.py
+++ homework/0317/5205_퀵정렬.py
@@ -1,56 +1,25 @@
-# def partition(left, right):
-#     pivot = arr[left]
-#     i = left + 1
-#     j = right
-#     
-#     while i <= j:
-#         while i <= j and arr[i] <= pivot:
-#             i += 1
-#         while i <= j and arr[j] >= pivot:
-#             j -= 1
-#             
-#         if i < j:
-#             arr[i], arr[j] = arr[j], arr[i]
-#             
-#     arr[left], arr[j] = arr[j], arr[left]
-#     return j
-# 
-# def quick_sort(left, right):
-#     if left < right:
-#         pivot = partition(left, right)
-#         quick_sort(left, pivot - 1)
-#         quick_sort(pivot + 1, right)
-#     
-# T = int(input())
-# for tc in range(1, T+1):
-#     N = int(input())
-#     arr = list(map(int, input().split()))
-#     quick_sort(0, N - 1)
-#     print(f'#{tc} {arr[N//2]}')
-
-def partitioning(left, right):
+def partition(left, right):
     pivot = arr[left]
     i = left + 1
     j = right
+
     while i <= j:
         while i <= j and arr[i] <= pivot:
             i += 1
         while i <= j and arr[j] >= pivot:
             j -= 1
-        
+
         if i < j:
             arr[i], arr[j] = arr[j], arr[i]
-    
+
     arr[left], arr[j] = arr[j], arr[left]
     return j
 
-
 def quick_sort(left, right):
     if left < right:
-        pivot = partitioning(left, right)
+        pivot = partition(left, right)
         quick_sort(left, pivot - 1)
         quick_sort(pivot + 1, right)
-        
 
 T = int(input())
 for tc in range(1, T+1):
