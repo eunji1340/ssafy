e6e04f03f1b1ecae3f34492daf88ea018bad9040
eunji <dmswl2781@naver.com>
2025-04-24 17:55:39 +0900

    0424

--- swexp/1383_점심식사.py
+++ swexp/1383_점심식사.py
@@ -1,6 +1,27 @@
+def move(time):
+    
+    if time 
+    
+    
+    
+
+    
+
+
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
+    stairs = []
+    peoples = []
+    min_time = float('inf')
+
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 1:
+                peoples.append([i, j])
+            elif arr[i][j] > 1:
+                stairs.append([i, j, arr[i][j]])
+
 
     print(f'#{tc}')
